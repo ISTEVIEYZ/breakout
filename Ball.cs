e282ada7fb162972138d0606ddc5d090a00505ed
Stephen Eisen <stephen.eisen27@gmail.com>
2020-02-26 23:48:23 -0500

    Added ball bounce and walls

--- Ball.cs
+++ Ball.cs
@@ -1,30 +1,40 @@
 using Godot;
 using System;
 
-public class Ball : RigidBody2D
+public class Ball : KinematicBody2D
 {
     // Declare member variables here. Examples:
     Vector2 screenSize;
-    Vector2 startPosition;
+
+    [Export] float speed = 300;
+
+    Vector2 velocity = new Vector2();
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         screenSize = GetViewport().Size;
-        startPosition = Position;
+        velocity = new Vector2(0, speed).Rotated(Rotation);
     }
 
     public override void _Process(float delta)
     {
 
     }
-    public override void _IntegrateForces(Physics2DDirectBodyState state)
+
+    public override void _PhysicsProcess(float delta)
     {
+        var collision = MoveAndCollide(velocity * delta);
+
+        if (collision != null)
+        {
+            velocity = velocity.Bounce(collision.Normal);
+        }
+        
         if (Position.x > screenSize.x || Position.x < 0 || Position.y > screenSize.y + 100)
         {
-            state.LinearVelocity = new Vector2(Mathf.Clamp(state.LinearVelocity.x, 5, 10), Mathf.Clamp(state.LinearVelocity.y, 5, 10));
-            state.Transform = new Transform2D(0, startPosition);
+           Position = new Vector2(100,100);
         }
-
+		    
     }
 }
